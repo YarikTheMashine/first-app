6472720aecb3f22da5f7b0fd333aac0602d4d4b2
YarikTheMashine <49390094+YarikTheMashine@users.noreply.github.com>
2019-07-19 16:58:13 +0300

    Updates

---
+++ precache-manifest.aa643f08478edb329f601906791b494c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c248bf0fe896850f665a0f437cedbe",
+    "url": "/welcome/index.html"
+  },
+  {
+    "revision": "58a7948e05d5ea65194d",
+    "url": "/welcome/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "6c850f4f4cdf8db0873d",
+    "url": "/welcome/static/css/main.5f2a04c0.chunk.css"
+  },
+  {
+    "revision": "58a7948e05d5ea65194d",
+    "url": "/welcome/static/js/2.54860a5d.chunk.js"
+  },
+  {
+    "revision": "6c850f4f4cdf8db0873d",
+    "url": "/welcome/static/js/main.595ef9f1.chunk.js"
+  },
+  {
+    "revision": "2c8cc3506e5cbcd658fc",
+    "url": "/welcome/static/js/runtime~main.82f1d66e.js"
+  },
+  {
+    "revision": "31e5912aa7eb2a8a5a265f8674c2f4ed",
+    "url": "/welcome/static/media/Stalemate.31e5912a.ttf"
+  },
+  {
+    "revision": "982b178c1ec55cbd350160c8d1482f73",
+    "url": "/welcome/static/media/background1.982b178c.jpg"
+  },
+  {
+    "revision": "67a831bbde60af6c2e4b17fe1416ee44",
+    "url": "/welcome/static/media/content3.67a831bb.jpg"
+  },
+  {
+    "revision": "9e5e57c478578e65ccf35d1d78a1e327",
+    "url": "/welcome/static/media/error2.9e5e57c4.gif"
+  },
+  {
+    "revision": "88406c693933b70024541674b39bfbf1",
+    "url": "/welcome/static/media/footer1.88406c69.jpg"
+  },
+  {
+    "revision": "efbbdc17d60946d5004ea89e22ac15bb",
+    "url": "/welcome/static/media/header2.efbbdc17.jpg"
+  },
+  {
+    "revision": "381e61759154fe2ff131fdb15347b213",
+    "url": "/welcome/static/media/logo.381e6175.jpg"
+  }
+]);
